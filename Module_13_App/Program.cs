ac496507a3d30142d4f7eac5dd5d7fcd909969c4
Alexander2437 <alexander.vtyurin@gmail.com>
2025-10-03 17:27:21 +0700

    Задание 13.1.4

--- Module_13_App/Program.cs
+++ Module_13_App/Program.cs
@@ -8,5 +8,24 @@ namespace Module_13_App
         {
         
         }
+
+        public bool GetSortType(int[] array)
+        {
+            if (array.First() < array.Last())
+                return true;
+            return false;
+        }
+
+        static bool CheckAscending(int[] numbers)
+        {
+            //  используем цикл for для обхода массива
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                //  проверяем следующий элемент на предмет того, что он меньше предыдушего
+                if (numbers[i + 1] < numbers[i])
+                    return false;
+            }
+            return true;
+        }
     }
 }
