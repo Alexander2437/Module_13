c3af4908860f3c4daee6a33cb5a60d4ec4169dda
Alexander2437 <alexander.vtyurin@gmail.com>
2025-10-10 18:54:50 +0700

    Тема 13.3.12

--- Module_13_App/Program.cs
+++ Module_13_App/Program.cs
@@ -8,31 +8,38 @@ namespace HashSetTest
     {
         static void Main(string[] args)
         {
-            var numberChars = new HashSet<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            var puntuationChars = new HashSet<char>() { '-', '.', ',', '!', '?', ' ' };
-
-            string sentence = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
-            var characters = sentence.ToCharArray();
-
-            var CharSet = new HashSet<char>();
-
-            foreach (char character in characters)
+            while (true)
             {
-                if (character != ' ')
-                    CharSet.Add(character);
-            }
-            Console.WriteLine("Number of symbols: {0}", CharSet.Count);
+                var numberChars = new HashSet<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                var puntuationChars = new HashSet<char>() { '-', '.', ',', '!', '?', ' ' };
 
-            CharSet.ExceptWith(puntuationChars);
-            Console.WriteLine("Number of symbols without punctuation marks: {0}", CharSet.Count);
+                Console.WriteLine("Введите строку:");
+                string sentence = Console.ReadLine();
 
-            if (CharSet.Overlaps(numberChars))
-            {
-                Console.WriteLine("The text contains numbers.");
-            }
-            else
-            {
-                Console.WriteLine("The text does not contain numbers.");
+                var characters = sentence.ToCharArray();
+
+                var CharSet = new HashSet<char>();
+
+                foreach (char character in characters)
+                {
+                    if (character != ' ')
+                        CharSet.Add(character);
+                }
+                Console.WriteLine("Number of symbols: {0}", CharSet.Count);
+
+                CharSet.ExceptWith(puntuationChars);
+                Console.WriteLine("Number of symbols without punctuation marks: {0}", CharSet.Count);
+
+                if (CharSet.Overlaps(numberChars))
+                {
+                    Console.WriteLine("The text contains numbers.");
+                }
+                else
+                {
+                    Console.WriteLine("The text does not contain numbers.");
+                }
+
+                Console.WriteLine();
             }
         }
     }
