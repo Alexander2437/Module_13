166e6ce71331ce54756dce6688f0a9cd2128d6e0
Alexander2437 <alexander.vtyurin@gmail.com>
2025-10-10 18:29:13 +0700

    Задание 13.3.11

--- Module_13_App/Program.cs
+++ Module_13_App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace HashSetTest
 {
@@ -7,18 +8,32 @@ namespace HashSetTest
     {
         static void Main(string[] args)
         {
+            var numberChars = new HashSet<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            var puntuationChars = new HashSet<char>() { '-', '.', ',', '!', '?', ' ' };
+
             string sentence = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
             var characters = sentence.ToCharArray();
-            
+
             var CharSet = new HashSet<char>();
 
-            foreach (char character in characters) 
+            foreach (char character in characters)
             {
                 if (character != ' ')
                     CharSet.Add(character);
             }
-
             Console.WriteLine("Number of symbols: {0}", CharSet.Count);
+
+            CharSet.ExceptWith(puntuationChars);
+            Console.WriteLine("Number of symbols without punctuation marks: {0}", CharSet.Count);
+
+            if (CharSet.Overlaps(numberChars))
+            {
+                Console.WriteLine("The text contains numbers.");
+            }
+            else
+            {
+                Console.WriteLine("The text does not contain numbers.");
+            }
         }
     }
 }
